03fa06ae9360bd88ee43624a1737c8108253ed6e
Abdias <felizseguraabdias@gmail.com>
2025-12-03 10:50:17 -0400

    Ok

---
+++ WebApplication2/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using ApplicacionClientComments.cs.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ClientController : Controller
+    {
+
+        private readonly IRepositoryApiClient _repositoryApiClient; 
+
+        public ClientController(IRepositoryApiClient repositoryApiClient)
+        {
+            _repositoryApiClient = repositoryApiClient;
+        }
+
+        [HttpGet]   
+        public async Task<IActionResult> Index()
+        {
+            var request = _repositoryApiClient.GetAllClientAsync();
+            return View(request);
+        }
+    }
+}
+
